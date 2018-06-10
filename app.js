6a40874ec85fb5e351772fe71afeb93efb5e019b
marcoguilherme <mgbm13@gmail.com>
2018-06-10 01:13:02 -0300

    Security applyied

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import routes from './src/routes/crmRoutes';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import jsonwebtoken from 'jsonwebtoken';
+import User from './src/models/userModel';
+
 
 let app = express();
 
@@ -12,6 +15,22 @@ mongoose.connect('mongodb://localhost/CRMdb')
 //bodyParser configuration
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
+
+//JWT Setup
+app.use((req, res, next) => {
+    if(req.headers &&
+        req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'JWT'){
+            jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', (err, decode) => {
+                if(err) req.user = undefined;
+                req.user = decode;
+                next();
+            })
+        }else{
+            req.user = undefined;
+            next();
+        }
+});
 
 routes(app);
 
